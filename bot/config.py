c0757305d7c8c3cd177c85f5fb9c14bfcb989122
knbro <81792272+knbro@users.noreply.github.com>
2021-10-25 15:38:51 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,9 @@
 class config:
-    BOT_TOKEN = "2014604363:AAGOaTQqvEaz_QWk1J7wkwwucIcdNfXdk_w"
+    BOT_TOKEN = "2068688415:AAElZ5Z5CZdjpGYb_J5ob_WL9Ajw5rXM3Dg"
     APP_ID = "8717645"
     API_HASH = "5df9431ec3c76e4c78a5f379d78081dc"
     DATABASE_URL = "postgres://pjynyrnqettwsi:1114b01516e75548db314072e13ccd67b41b94c8a04a5a870c26faecf3330f62@ec2-54-197-100-79.compute-1.amazonaws.com:5432/d48tfahekrkreh"
-    SUDO_USERS = "1859166311" # Sepearted by space.
+    SUDO_USERS = "1859166311 851259778" # Sepearted by space.
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = "448635037035-8r53agf6skc8lgobuuo9377r0gp9v55e.apps.googleusercontent.com"
     G_DRIVE_CLIENT_SECRET = "Jf5Sn1izbCBaciNKRWTUNuSi"
